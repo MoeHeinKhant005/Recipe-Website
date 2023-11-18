7e7280fe0e72e791405f7e7c9822aed2ac956873
MoeHeinKhant <moeheink578@gmail.com>
2023-11-18 10:08:32 +0630

    Add api keys and getData func
    Added API keys and getData function that will take a query parameter and fetch the datas from API using the parameter

---
+++ dist/script.js
@@ -0,0 +1,13 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8aec078454msh88978a01b27ef93p155bb3jsn4307bb337508',
+		'X-RapidAPI-Host': 'food-recipes-with-images.p.rapidapi.com'
+	}
+};
+
+async function getData(query){
+    const response = await fetch(`https://food-recipes-with-images.p.rapidapi.com/?q=${query}`);
+    const data = response.json();
+    return data;
+}
